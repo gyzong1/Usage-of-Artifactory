8d11726cc1e170be512c0551c8b910283363d1bb
gyzong1 <40457486+gyzong1@users.noreply.github.com>
2022-06-08 17:08:11 +0800

    Create maven-example-pipeline.groovy

---
+++ maven-example-pipeline.groovy
@@ -0,0 +1,30 @@
+node {
+
+  def artServer = Artifactory.server 'art1'
+  def rtMaven = Artifactory.newMavenBuild()
+  def buildInfo = Artifactory.newBuildInfo()
+  
+  stage ('Clone') {
+        git url: 'https://github.com/JFrog/project-examples.git'
+        // git url: 'https://gitee.com/gyzong1/project-examples.git'
+    }
+
+    stage ('Artifactory configuration') {
+        rtMaven.tool = 'maven3' // Tool name from Jenkins configuration
+        rtMaven.resolver releaseRepo: 'webinar-maven-virtual', snapshotRepo: 'webinar-maven-virtual', server: artServer
+        rtMaven.deployer releaseRepo: 'webinar-maven-dev-local', snapshotRepo: 'webinar-maven-dev-local', server: artServer
+    }
+
+    stage ('Env capture') {
+        buildInfo.env.capture = true
+    }
+
+    stage ('Exec Maven') {
+        rtMaven.run pom: 'maven-examples/maven-example/pom.xml', goals: 'clean install', buildInfo: buildInfo
+    }
+  
+    stage ('Publish build info') {
+        artServer.publishBuildInfo buildInfo
+    }
+
+}
